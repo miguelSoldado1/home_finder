3ca872f9b9e00d4797222f37c5a20dae5ab6ccdb
miguel <miguelsoldado12@gmail.com>
2024-05-17 22:07:21 +0100

    still trying things

--- api/index.go
+++ api/index.go
@@ -3,10 +3,12 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	imovirtual "example.com/home_finder_bot/Imovirtual"
 )
 
-// const priceMax = "800"
-// const locations = "[lisboa/lisboa/santa-clara,lisboa/lisboa/benfica,lisboa/lisboa/avenidas-novas,lisboa/lisboa/arroios,lisboa/lisboa/alvalade,lisboa/lisboa/penha-de-franca,lisboa/lisboa/olivais,lisboa/lisboa/lumiar,lisboa/lisboa/parque-das-nacoes,lisboa/lisboa/areeiro]"
+const priceMax = "800"
+const locations = "[lisboa/lisboa/santa-clara,lisboa/lisboa/benfica,lisboa/lisboa/avenidas-novas,lisboa/lisboa/arroios,lisboa/lisboa/alvalade,lisboa/lisboa/penha-de-franca,lisboa/lisboa/olivais,lisboa/lisboa/lumiar,lisboa/lisboa/parque-das-nacoes,lisboa/lisboa/areeiro]"
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	// err := godotenv.Load()
@@ -17,10 +19,13 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	// bot.BotToken = os.Getenv("BOT_TOKEN")
 	// bot.Run()
 
-	// messages := imovirtual.Search(priceMax, locations)
-	// for _, msg := range messages {
-	// 	bot.SendMessage("1241108323655356497", msg)
-	// }
+	fmt.Print("Starting bot...")
+	messages := imovirtual.Search(priceMax, locations)
+	for _, msg := range messages {
+		fmt.Println(msg)
+		// bot.SendMessage("1241108323655356497", msg)
+	}
+	fmt.Print("Finishing bot...")
 
 	// bot.Close()
 
