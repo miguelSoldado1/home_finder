84c31f57e1e0ca44c0d089aa6459b6eeed28aa88
miguel <miguelsoldado12@gmail.com>
2024-05-17 23:00:43 +0100

    adding a json response

--- api/index.go
+++ api/index.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,8 +12,24 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	main()
 	fmt.Println("Api triggered")
 
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]string)
+	resp["message"] = "Hello World from Go! 👋"
+	resp["language"] = "go"
+	resp["cloud"] = "Hosted on Vercel! ▲"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+
+	w.Write(jsonResp)
+}
+
+func main() {
 	bot_token := os.Getenv("BOT_TOKEN")
 
 	if bot_token == "" {
