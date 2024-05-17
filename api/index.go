a4ababbebd56b9131f046b12ddff5b099e33b9df
miguel <miguelsoldado12@gmail.com>
2024-05-17 21:54:10 +0100

    not defining .env name in load

--- api/index.go
+++ api/index.go
@@ -15,7 +15,7 @@ const priceMax = "800"
 const locations = "[lisboa/lisboa/santa-clara,lisboa/lisboa/benfica,lisboa/lisboa/avenidas-novas,lisboa/lisboa/arroios,lisboa/lisboa/alvalade,lisboa/lisboa/penha-de-franca,lisboa/lisboa/olivais,lisboa/lisboa/lumiar,lisboa/lisboa/parque-das-nacoes,lisboa/lisboa/areeiro]"
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	err := godotenv.Load(".env")
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("errrror message: ", err)
 	}
